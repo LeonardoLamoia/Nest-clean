d2f2838b64e695f7fc6fc128d2ef6ed2869dde48
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-09-18 23:10:42 -0300

    Protecting Routes with Guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { Env } from "src/env";
 
       },
     })
-  ]
+  ],
+  providers: [JwtStrategy],
 })
 export class AuthModule { }
