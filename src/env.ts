26dac5d322e397040bf21b87c654e8f90371e657
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-11-20 08:59:28 -0300

    environment variable correction

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-import { z } from "zod";
-
+import { z } from 'zod'
+import 'dotenv/config';
 
 export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
@@ -7,7 +8,7 @@ const prisma = new PrismaClient()
 
 function generateUniqueDatabaseURL(schemaId: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provider a DATABASE_URL environment variable.')
+    throw new Error('Please provider a DATABASE_URL environment variable')
   }
 
   const url = new URL(process.env.DATABASE_URL)
